30bc4bb1a127db90ab378ec34411e2595330e5fb
abiodun <abioduncsc@gmail.com>
2024-04-25 16:57:32 +0100

    cleanup: restructure component naming and remove unused variable and import

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -14,11 +14,6 @@ const Navbar = () => {
       <div>
         <div className="lg:inline text-n900 text-2xl font-semibold capitalize">{pathname.split("/").pop()}</div>
       </div>
-      <div className="flex items-center gap-5 pr-4">
-        <div className="flex items-center gap-5">      
-          <BellRing className="text-n900" size={20} />
-        </div>
-      </div>
     </div>
   );
 };
