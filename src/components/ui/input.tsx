f1ddec3e4420a92220093ab85539876be4da473a
abiodun <abioduncsc@gmail.com>
2024-04-25 17:25:49 +0100

    fix: auth form input padding

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -25,7 +25,7 @@ const Input: FC<InputProps> = ({label, id, type, required, register, errors, dis
           disabled={disabled}
           {...register(id, {required})}
           className={clsx(`
-            form-input block border-0 rounded-md w-full py-1.5 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6
+            form-input block border-0 rounded-md w-full py-1.5 px-3 text-gray-900  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6
           `, 
             errors[id] && 'focus:ring-rose-500',
             errors[id] && 'opacity-50 cursor-default'
